a7b4eda2c65451fb82dedec126d135f20d6b386f
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-12 06:40:01 +0200

    chore: refresh flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "wincrab — debloated Windows 11 ISO builder for Linux";
 
   inputs = {
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=c26b735eede8078f795651c4a9cbf0be8733b221";
+    rs-harbor.url = "git+https://codefloe.com/caniko/rs-harbor.git?ref=trunk&rev=7fa1c2104dab4e1dbaa1aaa6df84bba815aa282d";
     nixpkgs.follows = "rs-harbor/nixpkgs";
     crane.follows = "rs-harbor/crane";
 
